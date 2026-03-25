2015d6e8b930e90092abd3463b5d0859b62aeb2a
wr-nole99 <wmrttnole@gmail.com>
2026-03-25 22:46:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,11 @@ const gifStages = [
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
     "Pookie please... 🥺",
     "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
     "Please??? 💔",
     "Don't do this to me...",
     "Last chance! 😭",
-    "You can't catch me anyway 😜"
 ]
 
 const yesTeasePokes = [
